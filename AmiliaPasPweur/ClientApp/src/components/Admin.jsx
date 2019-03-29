7f61e0ecc7239bbf20e34a18ed79169e5dddb7ff
Alexandre Arpin <alexandre.arpin@gsoft.com>
2019-03-29 16:34:29 -0400

    Add dropdown to admin

--- AmiliaPasPweur/ClientApp/src/components/Admin.jsx
+++ AmiliaPasPweur/ClientApp/src/components/Admin.jsx
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { fetchAdminData } from '../actionCreators/adminActionCreator'
+import { fetchAvailableSports } from '../actionCreators/sweatActionCreator'
 import { bindActionCreators } from "redux";
 import LoadingPart from './parts/LoadingPart'
-import { Table } from 'semantic-ui-react'
 import GoogleMapReact from 'google-map-react'
+import { Dropdown } from "semantic-ui-react";
 
 class Admin extends Component {
   static defaultProps = {
@@ -15,6 +16,7 @@ class Admin extends Component {
     },
     zoom: 14
   }
+
   static propTypes = {
     fetchAdminData: PropTypes.func.isRequired,
     isFetched: PropTypes.bool.isRequired,
@@ -22,39 +24,58 @@ class Admin extends Component {
     notifications: PropTypes.array.isRequired,
     center: PropTypes.object.isRequired,
     zoom: PropTypes.number.isRequired,
+    fetchAvailableSports: PropTypes.func.isRequired,
+    areAvailableSportsFetched: PropTypes.bool.isRequired,
   };
+
+  state = {
+    selectedSport: null,
+  }
 
   componentDidMount() {
     if (!this.props.isFetched) {
       this.props.fetchAdminData()
     }
-  }
 
-  renderBodyRow(query) {
-    return (
-      <Table.Row key={query.sport}>
-        <Table.Cell>{query.sport}</Table.Cell>
-        <Table.Cell>{query.location.lat} | {query.location.lng}</Table.Cell>
-        <Table.Cell>{query.count}</Table.Cell>
-      </Table.Row>);
+    if (!this.props.areAvailableSportsFetched) {
+      this.props.fetchAvailableSports()
+    }
   }
 
   render() {
-    const { queries, isFetched } = this.props;
+    const { queries, isFetched, availableSports } = this.props;
+
+    const sportsContainedInQueries = Array.from(new Set(queries.map(x => x.sport)));
+
+    const sportsOptions = availableSports
+    .filter(x => sportsContainedInQueries.find(y => y === x.id))
+    .map(x => ({ key: x.id, text: x.name, value: x.id}));
+
 
     const heatMapData = {
-      positions: queries.filter(x => x.sport === 2).map(x => ({ lat: x.location.lat, lng: x.location.lng })),
+      positions: queries.filter(x => x.sport === this.state.selectedSport).map(x => ({ lat: x.location.lat, lng: x.location.lng })),
       options: {
         radius: 20,
         opacity: 0.7,
       }
     };
 
-    const apiKey = {key: 'AIzaSyADZ6SKjElEyIdZ7og8PzLEBZ6zLOAtPz8'}
+    console.log("heatmap", heatMapData);
+
+    const apiKey = { key: 'AIzaSyADZ6SKjElEyIdZ7og8PzLEBZ6zLOAtPz8' }
 
     return (
       <div>
         <h1>Administration</h1>
+        <div className="flex">
+          <Dropdown
+            options={sportsOptions}
+            selection
+            search
+            placeholder='Select Activity'
+            onChange={(event, data) => this.onSportSelect(data.value)}
+          />
+        </div>
         {!isFetched && <LoadingPart />}
         {
           isFetched &&
@@ -75,6 +96,13 @@ class Admin extends Component {
       </div >
     );
   }
+
+  onSportSelect = (value) => {
+    if (value) {
+      console.debug("Selected Sport", value)
+      this.setState({ selectedSport: value })
+    }
+  }
 }
 
 function mapStateToProps(state) {
@@ -82,12 +110,15 @@ function mapStateToProps(state) {
     isFetched: state.admin.isFetched,
     queries: state.admin.queries,
     notifications: state.admin.notifications,
+    availableSports: state.sweat.availableSports.sports,
+    areAvailableSportsFetched: state.sweat.availableSports.isFetched,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    fetchAdminData: bindActionCreators(fetchAdminData, dispatch)
+    fetchAdminData: bindActionCreators(fetchAdminData, dispatch),
+    fetchAvailableSports: bindActionCreators(fetchAvailableSports, dispatch)
   };
 }
 
