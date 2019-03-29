895a59ac50b0ca4d1d9247bc3f8322d692472cb7
Alexandre Arpin <alexandre.arpin@gsoft.com>
2019-03-29 14:48:57 -0400

    Show Results and loopback

--- AmiliaPasPweur/ClientApp/src/components/parts/ResultPart.jsx
+++ AmiliaPasPweur/ClientApp/src/components/parts/ResultPart.jsx
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from "redux";
+import { reset } from '../../actionCreators/sweatActionCreator'
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Button } from "semantic-ui-react";
 
-export default class ResultPart extends Component {
+class ResultPart extends Component {
+  static propTypes = {
+    results: PropTypes.array.isRequired,
+    reset: PropTypes.func.isRequired,
+  };
 
-  render () {
+  render() {
+    const { results } = this.props;
+
     return (
       <div>
         <h1>Congrats! Here are some results</h1>
-        <p>Welcome to your new single-page application, built with:</p>
+
+        <p>{results[0].fullName} - {results[0].telephone}</p>
+
+        <div>
+          <Button basic onClick={this.props.reset}>Start Again</Button>
+        </div>
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    results: state.sweat.results,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    reset: bindActionCreators(reset, dispatch),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ResultPart);
